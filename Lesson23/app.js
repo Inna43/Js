a0e1aac681d72dcc8a16655c0042528bb28b1b21
Inna43 <kotlyar@gymn14.net>
2022-01-23 16:42:13 +0200

    lesson 23

---
+++ Lesson23/app.js
@@ -0,0 +1,15 @@
+// alert ("Js это здорово!")
+
+// let name=prompt("Как тебя зовут?")
+// alert("Hello, " + name)
+
+/*let name=prompt("Как тебя зовут?")
+    console.log("Hello, " + name)*/
+    
+let iceCream=confirm("Ты любишь мороженое?")    
+    if (iceCream){
+        console.log ("Super!!!")
+    }
+    else{
+        console.log("^_^")
+    }

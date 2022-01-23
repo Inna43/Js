48c918dcfa4cbaae85d1f27383f181be41296483
Inna43 <kotlyar@gymn14.net>
2022-01-23 14:32:57 +0200

    js

---
+++ Lesson 1/app.js
@@ -0,0 +1,23 @@
+// window.onload = function () {
+//   let i = 5;
+//   let timer = setInterval(function () {
+//         i--;
+//         document.getElementById('timer').innerHTML = 'Осталось секунд: ' + i;
+//         if (i == 0) {
+//             document.getElementById('example').style.display = 'block';
+//             document.getElementById('timer').style.display = 'none';
+//             clearInterval(timer);
+//         }
+//     }, 1000)
+// }
+
+
+alert ('Ответь на несколько вопросов, yes/no')
+
+
+
+let mult=prompt('Ты смотришь мультик "Губка Боб?')
+
+   if (mult=='yes')     alert('Это точно?');
+   if (mult=='no')      alert('Это грустно :-(');
+
